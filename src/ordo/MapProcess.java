a7880e39efa0fe40fe675186b190c255c6b76eca
MOUDDENEHamza <hz.mouddene@gmail.com>
2021-02-21 23:36:29 +0100

    #release

--- src/ordo/MapProcess.java
+++ src/ordo/MapProcess.java
@@ -1,37 +1,37 @@
-//package ordo;
-//
-//import formats.Format;
-//import map.Mapper;
-//
-//public class MapProcess implements Runnable {
-//	Mapper m;
-//	Format reader;
-//	Format writer;
-//	CallBack cb;
-//
-//	public MapProcess(Mapper m, Format reader, Format writer, CallBack cb) {
-//		this.m = m;
-//		this.reader = reader;
-//		this.writer = writer;
-//		this.cb = cb;
-//	}
-//
-//	public void run() {
-//		// Open reader and writer
-//		reader.open(Format.OpenMode.R);
-//		writer.open(Format.OpenMode.W);
-//
-//		// Launch map
-//		m.map(reader, writer);
-//
-//		// Close reader and writer
-//		reader.close();
-//		writer.close();
-//
-//		// Report that the process has finished runMap task.
-//		cb.runMapDone();
-//	}
-//
-//}
+package ordo;
+
+import formats.Format;
+import map.Mapper;
+
+public class MapProcess implements Runnable {
+	Mapper m;
+	Format reader;
+	Format writer;
+	CallBack cb;
+
+	public MapProcess(Mapper m, Format reader, Format writer, CallBack cb) {
+		this.m = m;
+		this.reader = reader;
+		this.writer = writer;
+		this.cb = cb;
+	}
+
+	public void run() {
+		// Open reader and writer
+		reader.open(Format.OpenMode.R);
+		writer.open(Format.OpenMode.W);
+
+		// Launch map
+		m.map(reader, writer);
+
+		// Close reader and writer
+		reader.close();
+		writer.close();
+
+		// Report that the process has finished runMap task.
+		cb.runMapDone();
+	}
+
+}
 
 
--- src/ordo/WorkerImpl.java
+++ src/ordo/WorkerImpl.java
@@ -20,37 +20,6 @@ import java.rmi.server.UnicastRemoteObject;
  */
 public class WorkerImpl extends UnicastRemoteObject implements Worker {
 
-    public class MapProcess implements Runnable {
-        Mapper m;
-        Format reader;
-        Format writer;
-        CallBack cb;
-
-        public MapProcess(Mapper m, Format reader, Format writer, CallBack cb) {
-            this.m = m;
-            this.reader = reader;
-            this.writer = writer;
-            this.cb = cb;
-        }
-
-        public void run() {
-            // Open reader and writer
-            reader.open(Format.OpenMode.R);
-            writer.open(Format.OpenMode.W);
-
-            // Launch map
-            m.map(reader, writer);
-
-            // Close reader and writer
-            reader.close();
-            writer.close();
-
-            // Report that the process has finished runMap task.
-            cb.runMapDone();
-        }
-
-    }
-
     /**
      * Attributes of WorkerImpl class
      */
@@ -58,7 +27,7 @@ public class WorkerImpl extends UnicastRemoteObject implements Worker {
     static int id;              // The Id of the worker
     static String url;          // The url of the worker
     Registry registry;
-    
+
     /**
      * Constructor of WorkerImpl class that creates a worker
      *
