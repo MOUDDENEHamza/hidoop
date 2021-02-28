c41c6c3b5b3f82ff2ca1f9eee81385490c399da1
MOUDDENEHamza <hz.mouddene@gmail.com>
2021-02-28 21:15:32 +0100

    #update run.sh and user.sh and make user.sh executable

--- run.sh
+++ run.sh
@@ -1,39 +1,56 @@
 #!/bin/bash
 
-HOSTS=(
-  ader.enseeiht.fr iode.enseeiht.fr carbone.enseeiht.fr oxygene.enseeiht.fr sodium.enseeiht.fr neon.enseeiht.fr
-  azote.enseeiht.fr fluor.enseeiht.fr yoda.enseeiht.fr solo.enseeiht.fr vador.enseeiht.fr dragon.enseeiht.fr
-  aston.enseeiht.fr cyclope.enseeiht.fr fermat.enseeiht.fr luke.enseeiht.fr
-)
+NAMEPROVIDER=( ader.enseeiht.fr)
+SERVERS=(
+        iode.enseeiht.fr carbone.enseeiht.fr oxygene.enseeiht.fr sodium.enseeiht.fr neon.enseeiht.fr
+        azote.enseeiht.fr fluor.enseeiht.fr
+        )
+WORKERS=(
+        yoda.enseeiht.fr solo.enseeiht.fr vador.enseeiht.fr dragon.enseeiht.fr
+        aston.enseeiht.fr cyclope.enseeiht.fr fermat.enseeiht.fr
+        )
+USER=( luke.enseeiht.fr )
 
 run_hdfs() {
-  terminator --title NameProvider -e "ssh $login@${HOSTS[0]} 'cd nosave/hadoop && make compile && java -cp src hdfs.NameProvider'; exec bash"
+  terminator --title NameProvider -e "ssh $login@${NAMEPROVIDER[0]} 'cd nosave/hadoop && make compile && java -cp src hdfs.NameProvider'; exec bash"
   sleep 5
-  for ((i = 1; i <= $nb_servers; i++)); do
-    terminator --title Server"$i" -e "ssh $login@${HOSTS[$i]} 'cd nosave/hadoop && java -cp src:lib/snakeyaml-1.5.jar hdfs.HdfsServer server$i 147.127.135.160'; exec bash"
+  for ((i = 0; i < "$nb_servers"; i++)); do
+    terminator --title Server"$(("$i" + 1))" -e "ssh $login@${SERVERS[$i]} 'cd nosave/hadoop && java -cp src:lib/snakeyaml-1.5.jar hdfs.HdfsServer server$(("$i" + 1)) 147.127.135.160'; exec bash"
     sleep 2
   done
 }
 
 run_hidoop() {
-  for ((i = 1; i <= $nb_servers; i++)); do
-    terminator --title Worker"$i" -e "ssh $login@${HOSTS[$nb_servers + i]} 'cd nosave/hadoop && java -cp src ordo.WorkerImpl 800$i $i'; exec bash"
+  for ((i = 0; i < "$nb_servers"; i++)); do
+    terminator --title Worker"$(("$i" + 1))" -e "ssh $login@${WORKERS[$i]} 'cd nosave/hadoop && java -cp src ordo.WorkerImpl 800$(("$i" + 1)) $(("$i" + 1))'; exec bash"
     sleep 2
   done
 }
 
-# Get user login
+# Greeting message
 clear
+echo "HADOOP"
+echo
+
+# Get user login
+
 read -rp "Please type your login : " login
 echo
 
 # Kill name provider process running
-ssh "$login@${HOSTS[0]}" "pkill java"
+ssh "$login@${NAMEPROVIDER[0]}" "pkill java"
 
 # Get the number of servers
-echo "We have $(($((${#HOSTS[@]} - 2)) / 2)) servers, please choose at least 1 server and at most "
-echo "$(($((${#HOSTS[@]} - 2)) / 2)) servers."
-read -rp "Please type the number of servers : " nb_servers
+echo "We have ${#SERVERS[@]} servers, please choose at least 1 server and at most ${#SERVERS[@]} servers."
+while true; do
+  read -rp "Please type the number of servers : " nb_servers
+  if [[ $nb_servers ]] && [ "$nb_servers" -eq "$nb_servers" ] 2>/dev/null; then
+    break
+  else
+    echo $'\e[31;1mERROR :\e[0m\eT '"$nb_servers"' is not an integer or not defined'
+    echo "Please try again"
+  fi
+done
 
 # Run the name provider and the servers
 run_hdfs
@@ -42,4 +59,4 @@ run_hdfs
 run_hidoop
 
 # Run the application
-terminator --title User -e "ssh $login@${HOSTS[${#n7_computers[@]} - 1]} 'cd nosave/hadoop && ./user.sh'; exec bash"
+terminator --title User -e "ssh $login@${USER[0]} 'cd nosave/hadoop && ./user.sh'; exec bash"
--- user.sh
+++ user.sh
@@ -1,9 +1,46 @@
 #!/bin/bash
 
-echo "Hello $HOSTNAME, if it is the first time you run this application, please mind that before you launch this script"
-echo " you should write a file in the servers you will use."
-echo "Tips : To generate a text file you can use generate_data.sh script in data folder before you run the application."
+# Print greeting message
+echo "HADOOP"
 echo
+echo "Hello $HOSTNAME, if it is the first time you run this application, please mind that before you launch this script"
+echo "you should write a file in the servers you will use."
+echo $'\e[33;1mTips :\e[0m\eT To generate a text file you can use generate_data.sh script in data folder before you '
+echo "run the application."
 
-sleep 5
-java -cp src application.MyMapReduce data/data.txt
+# Propose a menu allowing user execute different options
+while true; do
+  # print Usage and get a choice from user
+  echo
+  echo "To exit please type 0"
+  echo "To write a text file to servers type 1"
+  echo "To run the application please type 2"
+  echo
+  read -rp "Please type your choice : " choice
+
+  # Handle the input if it is an integer
+  if [[ $choice ]] && [ "$choice" -eq "$choice" ] 2>/dev/null; then
+    # To generate text file
+    if [[ $choice -eq 1 ]]; then
+      ./data/generate_data.sh
+    # Run the application
+    elif [[ $choice -eq 2 ]]; then
+      sleep 5
+      java -cp src application.MyMapReduce data/data.txt
+    # Exit the script
+    elif [[ $choice -eq 0 ]]; then
+      echo
+      echo "Goodbye"
+      break
+    # if the choice is greater than 2 or lower than 0
+    else
+      echo $'\e[31;1mERROR :\e[0m\eT Wrong input, please type 0, 1 or 2.'
+      echo "Please try again"
+    fi
+  # Handle the input if it is not an integer
+  else
+    echo $'\e[31;1mERROR :\e[0m\eT '"$choice"' is not an integer or not defined'
+    echo "Please try again"
+  fi
+done
+echo
