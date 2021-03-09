b78407303239a560253b170b912bf37bfbba07dd
MOUDDENEHamza <hz.mouddene@gmail.com>
2021-03-09 15:45:51 +0100

    #

--- test/Client.java
+++ test/Client.java
@@ -24,7 +24,7 @@ public class Client {
         System.out.println("Host = "+ args[0] + "    Port = 8000");
         while (true) {
             System.out.println("Start connection with server");
-            client.startConnection("127.0.0.1", 8000);
+            client.startConnection(args[0], 8000);
             System.out.println("Send heart beat to server");
             out.println(args[0]);
             Thread.sleep(2000);
