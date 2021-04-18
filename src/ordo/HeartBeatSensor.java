cc2c1d1e4c5fe15dc07fcab66857b7ef7d57c33d
hamza <hz.mouddene@gmail.com>
2021-04-18 17:34:40 +0200

    #

--- src/ordo/HeartBeatSensor.java
+++ src/ordo/HeartBeatSensor.java
@@ -39,6 +39,7 @@ public class HeartBeatSensor {
                         System.out.println("Worker on port " + (8000 + i + 1) + " down.");
                         System.out.println("Reboot worker on port " + (8000 + i + 1) + ".");
                         Runtime.getRuntime().exec("./relaunch.sh " + Hosts.workersIP[i] + " " + i);
+                        System.out.println("Rebooting worker on port " + (8000 + i + 1) + " done with success.");
                     }
                     Thread.sleep(1000);
                 }
