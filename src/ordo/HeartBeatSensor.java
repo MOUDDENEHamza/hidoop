6f285da5c4722bb44afd93e49763dd4e4787c7bf
hamza <hz.mouddene@gmail.com>
2021-04-18 17:48:02 +0200

    #

--- src/ordo/HeartBeatSensor.java
+++ src/ordo/HeartBeatSensor.java
@@ -25,10 +25,10 @@ public class HeartBeatSensor {
         // -- Linux --
 
         // Run a shell command
-        processBuilder.command("bash", "-c", "ssh hmoudden@succube.enseeiht.fr/147.127.133.3 'cd nosave/hadoop && java -cp src ordo.WorkerImpl 8001 1'");
+        //processBuilder.command("bash", "-c", "ssh hmoudden@succube.enseeiht.fr/147.127.133.3 'cd nosave/hadoop && java -cp src ordo.WorkerImpl 8001 1'");
 
         // Run a shell script
-        //processBuilder.command("path/to/hello.sh");
+        processBuilder.command("relaunch.sh");
 
         try {
 
