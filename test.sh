75f3517f880e09ff7f487e0a7866bae1883ef824
hamza <hz.mouddene@gmail.com>
2021-04-18 17:12:55 +0200

    #

--- test.sh
+++ test.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-ssh hmoudden@$1 "cd nosave/hadoop && java -cp src ordo.WorkerImpl 800$(("$2" + 1)) $(("$2" + 1))"
+ssh hmoudden@$1 "cd nosave/hadoop && java -cp src ordo.WorkerImpl 800$(("$2" + 1)) $(("$2" + 1))" &
+kill -INT 888
