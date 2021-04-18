5cc855bfd218a15b6ff635364931a8ce2a6edf75
hamza <hz.mouddene@gmail.com>
2021-04-18 18:34:45 +0200

    #

--- src/ordo/HeartBeatSensor.java
+++ src/ordo/HeartBeatSensor.java
@@ -29,36 +29,6 @@ public class HeartBeatSensor {
 
         // Run a shell script
         processBuilder.command("./relaunch.sh");
-
-        try {
-
-            Process process = processBuilder.start();
-
-            StringBuilder output = new StringBuilder();
-
-            BufferedReader reader = new BufferedReader(
-                    new InputStreamReader(process.getInputStream()));
-
-            String line;
-            while ((line = reader.readLine()) != null) {
-                output.append(line + "\n");
-            }
-
-            int exitVal = process.waitFor();
-            if (exitVal == 0) {
-                System.out.println("Success!");
-                System.out.println(output);
-                System.exit(0);
-            } else {
-                System.out.println("Failed!" + exitVal);
-                System.out.println(output);
-            }
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
     }
 
     /**
