3e7aaad8adb9aa628cc537e14469d447d5833cdb
hamza <hz.mouddene@gmail.com>
2021-04-27 17:32:52 +0200

    #

--- src/ordo/HeartBeatSensor.java
+++ src/ordo/HeartBeatSensor.java
@@ -106,9 +106,13 @@ public class HeartBeatSensor {
                         Thread.sleep(2000);
                         System.out.println("test : " + (jobState != State.UP));
                         if (jobState != State.UP) {
+                            System.out.println("1");
                             registry2 = LocateRegistry.getRegistry("behemot.enseeiht.fr", 9999);
+                            System.out.println("2");
                             JobInterface job = (JobInterface) registry2.lookup("//localhost:9999/Job");
+                            System.out.println("3");
                             job.relaunchJob(mr);
+                            System.out.println("4");
                         }
                     }
                     Thread.sleep(500);
